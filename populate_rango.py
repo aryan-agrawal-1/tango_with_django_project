38700509592af5a38f3a7625c22bbc5e552c02b8
“Firstname <2877689a@student.gla.ac.uk>
2025-01-26 16:00:14 +0000

    Chapter 5 complete

--- populate_rango.py
+++ populate_rango.py
@@ -51,9 +51,9 @@ def populate():
     ]
 
     cats = {
-        'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages}
+        'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
     }
 
     def add_page(cat, title, url, views = 0):
@@ -63,13 +63,13 @@ def populate():
         p.save()
         return p
     
-    def add_cat(name):
-        c = Category.objects.get_or_create(name=name)[0]
+    def add_cat(name, views, likes):
+        c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
         c.save()
         return c
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
